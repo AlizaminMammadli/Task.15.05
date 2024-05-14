eb29e41c1f34f100a62120335eff11f49ce331d5
Alizamin23 <alizaminam@code.edu.az>
2024-05-15 03:30:01 +0400

    Add task 15.05

---
+++ Products/Products/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var product1 = new { Id = 123, Name = "Cup", Price = 00, StockCount = 76 };
+            var product2 = new { Id = 234, Name = "Spoon", Price = 00, StockCount = 27 };
+            var product3 = new { Id = 345, Name = "Frying Pan", Price = 00, StockCount = 4 };
+            double numaver = 0;
+
+            var products = new[] { product1, product2, product3 };
+
+            foreach (var product in products)
+            {
+                numaver += product.Price;
+            }
+
+            numaver = numaver / products.Length;
+
+            Console.WriteLine($"The price numerical average is - {numaver} Euro" );
+        }
+    }
+}
---
+++ The largest number/The largest number/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace The_largest_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 19, 2, 25, 455 };
+
+            int theLargest = number[0];
+            for (int i = 1; i < number.Length; i++)
+            {
+                if (theLargest < number[i])
+                {
+                    theLargest = number[i];
+                }
+            }
+            Console.WriteLine("The Largest Number is" + " " + theLargest);
+        }
+    }
+}
